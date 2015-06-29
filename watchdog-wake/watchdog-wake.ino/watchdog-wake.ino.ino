7c90bdf39ea206d22a82b1c2470da25c329bb1ae
Pedro Liska <pedroliska@gmail.com>
2015-06-28 22:09:42 -0600

    initial modification, it doesn't work yet

--- watchdog-wake/watchdog-wake.ino/watchdog-wake.ino.ino
+++ watchdog-wake/watchdog-wake.ino/watchdog-wake.ino.ino
@@ -1,73 +1,24 @@
-/*-----------------------------------------------------------------------*
- * millionOhms.cpp -- "Warning" Lights for ATtiny85/45.                  *
- * Flashes four LEDs, different speeds and patterns are selected by      *
- * pressing the SELECT button.  Sleeps after five minutes to conserve    *
- * battery power (2xAA).  A long press on the SELECT button will also    *
- * initiate sleep mode.                                                  *
- *                                                                       *
- * Set fuse bytes: L=0x62 H=0xDE E=0xFF                                  *
- * (BOD is set at 1.8V, system clock is 1MHz, from the internal 8MHz RC  *
- * oscillator divided by 8.  Low and Extended fuses remain at their      *
- * factory default values.)                                              *
- *                                                                       *
- * Jack Christensen 18Sep2011 v1                                         *
- * 18Jan2013 v2 Added random mode which uses the watchdog timer to wake  *
- *           the MCU and flash the LEDs at random intervals. Invoke      *
- *           random mode by pressing RESET and SELECT simultaneously,    *
- *           first release RESET, and then release SELECT. Press and     *
- *           release RESET alone to return to normal (manual) mode.     *
- *                                                                       *
- * This work is licensed under the Creative Commons Attribution-         *
- * ShareAlike 3.0 Unported License. To view a copy of this license,      *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a        *
- * letter to Creative Commons, 171 Second Street, Suite 300,             *
- * San Francisco, California, 94105, USA.                                *
- *-----------------------------------------------------------------------*/
-
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
-#include <Button.h>              //http://github.com/JChristensen/Button
 #ifndef ARDUINO
 #include <stdint.h>
 #include "millionUtil.h"         //not needed if compiled with Arduino & Arduino-Tiny
 #endif
 
-#define RANDOM_RUN_MS 16000      //milliseconds to stay awake for in random mode (16 sec)
-#define MANUAL_RUN_MS 300000     //milliseconds to stay awake for in manual mode (5 min)
-#define RANDOM_SLEEP_MIN 73      //minimum number of 8-sec wdt periods to sleep for in random mode (~10 min)
-#define RANDOM_SLEEP_MAX 220     //maximum number of 8-sec wdt periods to sleep for in random mode (~30 min)
-#define LED1 1                   //PB1 LED1
-#define LED2 0                   //PB0 LED2
-#define LED3 3                   //PB3 LED3
-#define LED4 4                   //PB4 LED4
-#define BUTTON 2                 //PB2
-#define delayBase 125U           //ms between changing LEDs
-#define TACT_DEBOUNCE 25         //debounce time for tact switches in ms
-#define LONG_PRESS 1000      //milliseconds for a long button press
-#define BODS 7                   //BOD Sleep bit in MCUCR
-#define BODSE 2                  //BOD Sleep enable bit in MCUCR
-#define nPATTERNS 8U             //number of display patterns
-#define nSPEEDS 3U               //number of display speeds
-#define MAX_MODE (nPATTERNS * nSPEEDS) - 1    //encode speeds and patterns into a single number
+int PIN = 1;
+int REGULAR_LIGHT_MS = 3000;
+int WAKE_INDICATOR_LIGHT_MS = 1000;
 
-//function prototypes
-void setup(void);
-void loop(void);
-void goToSleep(void);
-void wdtEnable(void);
-void wdtDisable(void);
-void runLEDs(void);
-void setPinsOutput(void);
-void setPinsInput(void);
+//int RANDOM_SLEEP_MIN = 73;      //minimum number of 8-sec wdt periods to sleep for in random mode (~10 min)
+//int RANDOM_SLEEP_MAX = 220;     //maximum number of 8-sec wdt periods to sleep for in random mode (~30 min)
+int RANDOM_SLEEP_MIN = 1;
+int RANDOM_SLEEP_MAX = 2;
 
-Button btn = Button(BUTTON, true, true, TACT_DEBOUNCE);    //instantiate the tact switch
 uint8_t ledState, mode, mcucr1, mcucr2;
-bool randomMode;                     //flag if we're in random wake mode (via WDT)
-volatile bool buttonWake;            //flag whether the button woke us as opposed to the WDT
 bool keepSleeping;                   //flag to keep sleeping or not in random mode
 unsigned long msNow;                 //the current time from millis()
 unsigned long msLast;                //the last time the LEDs were changed
@@ -75,47 +26,18 @@ unsigned long msWakeUp;              //the time we woke up
 unsigned long msRunTime;             //how long to stay awake for
 long wdtCount;                       //how many 8-sec WDT periods we've slept for
 long wdtLimit;                       //number of WDT periods to wake after
-uint8_t randLED;                     //for the random case
 
-#ifndef ARDUINO
-int main(void)               //main() not needed in Arduino environment
-{
-    setupUtil();
-    setup();
-    loop();
-}
-#endif
-
-void setup(void)
-{
-    wdt_reset();
-    wdtDisable();
-    setPinsOutput();
-    randomMode = btn.read();                //invoke random wake mode if button pressed
-    while (btn.isPressed()) btn.read();     //wait for the button to be released
-    msRunTime = randomMode ? RANDOM_RUN_MS : MANUAL_RUN_MS;
-    wdtLimit = random(RANDOM_SLEEP_MIN, RANDOM_SLEEP_MAX + 1);
+void setup() {
+//  wdt_reset();
+//  wdtDisable();
+//  setPinsOutput();
 }
 
-void loop(void)
-{
-    for (;;) {
-        msNow = millis();
-        btn.read();
-        if (btn.wasReleased()) {
-            if (++mode > MAX_MODE) mode = 0;
-        }
-        else if (btn.pressedFor(LONG_PRESS)) {
-            setPinsInput();
-            while (!btn.wasReleased()) btn.read();    //wait for the button to be released
-            goToSleep();
-        }
-        else if (msNow - msWakeUp >= msRunTime) {
-            setPinsInput();
-            goToSleep();
-        }
-        runLEDs();
-    }
+void loop() {
+//  msNow = millis();
+//  goToSleep();
+  blinkLed(REGULAR_LIGHT_MS);
+  delay(1000);
 }
 
 void goToSleep(void)
@@ -127,7 +49,9 @@ void goToSleep(void)
         ADCSRA &= ~_BV(ADEN);                     //disable ADC
         set_sleep_mode(SLEEP_MODE_PWR_DOWN);
         sleep_enable();
-        if (randomMode) wdtEnable();              //start the WDT if we're in random mode
+        
+        wdtEnable();              //start the WDT
+        
         //turn off the brown-out detector.
         //must have an ATtiny45 or ATtiny85 rev C or later for software to be able to disable the BOD.
         //current while sleeping will be <0.5uA if BOD is disabled, <25uA if not.
@@ -144,44 +68,24 @@ void goToSleep(void)
         sleep_disable();
         wdtDisable();                  //don't need the watchdog while we're awake
         sei();                         //enable interrupts again (but INT0 is disabled above)
-        if (buttonWake) {              //what woke us
-            keepSleeping = false;      //the button woke us, so turn the lights on again
-            msRunTime = MANUAL_RUN_MS;
-            btn.read();
-            while (!btn.wasReleased()) btn.read();        //wait for the button to be released
-        }
-        else if (randomMode && (++wdtCount < wdtLimit)) {
+
+        if (++wdtCount < wdtLimit) {
             keepSleeping = true;
-            //setPinsOutput();            //briefly blink an LED so we can see the wdt wake-ups
-            //digitalWrite(LED4, HIGH);
-            //_delay_ms(10);
-            //setPinsInput();
+            blinkLed(WAKE_INDICATOR_LIGHT_MS);            //briefly blink an LED so we can see the wdt wake-ups
         }
         else {
             keepSleeping = false;
-            msRunTime = RANDOM_RUN_MS;
-            if (++mode > MAX_MODE) mode = 0;
         }
     } while (keepSleeping);
     
     setPinsOutput();
-    buttonWake = false;
     msWakeUp = millis();
-    if (randomMode) {              //set up for next time
-        wdtCount = 0;
-        wdtLimit = random(RANDOM_SLEEP_MIN, RANDOM_SLEEP_MAX + 1);
-    }
+
+    wdtCount = 0;     //set up for next time
+    wdtLimit = random(RANDOM_SLEEP_MIN, RANDOM_SLEEP_MAX + 1);
 }
 
-ISR(INT0_vect)
-{
-    buttonWake = true;            //flag that the button woke us up (and not the wdt)
-    GIMSK = 0x00;                 //disable INT0
-}    
-
-ISR(WDT_vect) {}                  //don't need to do anything here when the WDT wakes the MCU
-
-//enable the wdt for 8sec interrupt
+//enable the WDT for 8sec interrupt
 void wdtEnable(void)
 {
     wdt_reset();
@@ -192,7 +96,8 @@ void wdtEnable(void)
     sei();
 }
 
-//disable the wdt
+
+//disable the WDT
 void wdtDisable(void)
 {
     wdt_reset();
@@ -203,173 +108,20 @@ void wdtDisable(void)
     sei();
 }
 
-void runLEDs(void)
-{
-    if ( msNow - msLast >= delayBase << (mode % nSPEEDS)) {
-        msLast = msNow;
-        switch (mode / nSPEEDS) {
-
-            case 0:                        // o  -    -  o
-                switch (ledState % 2) {    // -  o    o  -
-                    case 0:
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED1, HIGH);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED2, HIGH);
-                        digitalWrite(LED3, HIGH);
-                        break;
-                }
-                break;
-
-            case 1:                        // o  -    -  o    -  -    -  -
-                switch (ledState) {        // -  -    -  -    o  -    -  o
-                    case 0:
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED1, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED2, HIGH);
-                        break;
-                    case 2:
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED3, HIGH);
-                        break;
-                    case 3:
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                }
-                break;
-
-            case 2:                        // o  o    -  -
-                switch (ledState % 2) {    // -  -    o  o
-                    case 0:
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED1, HIGH);
-                        digitalWrite(LED2, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED3, HIGH);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                }
-                break;
-
-            case 3:                        // o  -    -  o    -  -    -  -
-                switch (ledState) {        // -  -    -  -    -  o    o  -
-                    case 0:
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED1, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED2, HIGH);
-                        break;
-                    case 2:
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                    case 3:
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED3, HIGH);
-                        break;
-                }
-                break;
-
-            case 4:                        // o  o    -  -
-                switch (ledState % 2) {    // o  o    -  -
-                    case 0:
-                        digitalWrite(LED1, HIGH);
-                        digitalWrite(LED2, HIGH);
-                        digitalWrite(LED3, HIGH);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED4, LOW);
-                        break;
-                }
-                break;
-
-            case 5:                        // o  -    -  -    -  -    -  o
-                switch (ledState) {        // -  -    o  -    -  o    -  -
-                    case 0:
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED1, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED3, HIGH);
-                        break;
-                    case 2:
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                    case 3:
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED2, HIGH);
-                        break;
-                }
-                break;
-
-            case 6:                        // o  -    -  o
-                switch (ledState % 2) {    // o  -    -  o
-                    case 0:
-                        digitalWrite(LED2, LOW);
-                        digitalWrite(LED4, LOW);
-                        digitalWrite(LED1, HIGH);
-                        digitalWrite(LED3, HIGH);
-                        break;
-                    case 1:
-                        digitalWrite(LED1, LOW);
-                        digitalWrite(LED3, LOW);
-                        digitalWrite(LED2, HIGH);
-                        digitalWrite(LED4, HIGH);
-                        break;
-                }
-                break;
-
-            case 7:                            //random
-                randLED = random(0, 4);        //a number between 0 and 3
-                if (randLED > 1) ++randLED;    //maps 0,1,2,3 to 0, 1, 3, 4
-                digitalWrite(LED1, LOW);
-                digitalWrite(LED2, LOW);
-                digitalWrite(LED3, LOW);
-                digitalWrite(LED4, LOW);
-                digitalWrite(randLED, HIGH);
-                break;
-        }
-        if (++ledState > 3) ledState = 0;
-    }
-}
-
 void setPinsOutput(void)
 {
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    pinMode(LED3, OUTPUT);
-    pinMode(LED4, OUTPUT);
+    pinMode(PIN, OUTPUT);
 }
 
 void setPinsInput(void)
 {
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED4, LOW);
-    pinMode(LED1, INPUT);
-    pinMode(LED2, INPUT);
-    pinMode(LED3, INPUT);
-    pinMode(LED4, INPUT);
+    digitalWrite(PIN, LOW);
 }
+
+void blinkLed (int secondsOfLight) {
+  setPinsOutput();            //briefly blink an LED so we can see the wdt wake-ups
+  digitalWrite(PIN, HIGH);
+  delay(secondsOfLight * (unsigned long)1000);
+  setPinsInput();
+}
+
